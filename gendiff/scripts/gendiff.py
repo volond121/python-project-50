7ae6fd9e183c4d797f4d6ae8f845469bb343b78c
Vadim Sotnikov <602587@list.ru>
2024-08-22 15:35:19 +0300

    step two complete

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('first_file', metavar='first_file', type=str)
 parser.add_argument('second_file', metavar='second_file', type=set)
+parser.add_argument('-f', '--format', metavar='FORMAT', help='set format of output')
 args = parser.parse_args()
 
 
